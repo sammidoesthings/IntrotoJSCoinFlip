a18d3f3237e8c9ed5a5c35c5587f8ece7a4e60e1
Samantha <88806866+sammidoesthings@users.noreply.github.com>
2021-08-30 15:07:16 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+for (let i = 0; i < 10; i++){
+
+var coinFlip = Math.round(Math.random());
+
+if (coinFlip === 1) {
+  console.log("heads");
+} else if (coinFlip === 0) {
+  console.log("tails");
+}
+}
